14f21478fe419644c4d72d8ce99089d0cbc63477
Juan Carlos <jccolombian@gmail.com>
2023-03-27 21:52:03 -0500

    Crear commit inicial, 4 listas, mostrar su tamaño, y .gitignore

---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+Hasta ahora hemos
+trabajado con variables
+que permiten almacenar
+un único valor
+'''
+
+edad = 12
+
+altura = 1.79
+
+nombre = "Juan"
+
+'''
+En python podemos
+usar una variable
+que almacena una
+colección de datos
+y luego accederla
+usando un subíndice
+'''
+
+# lista de enteros
+lista1 = [10, 5, 3, 9]
+# lista de decimales
+lista2 = [1.78, 2.66, 1.55, 89.4]
+# lista de string
+lista3 = ["Lunes","Martes","Miercolés"]
+'''
+lista de elementos
+de distinto tipo
+'''
+lista4 = ["juan",45,1.92]
+
+
+if __name__ == '__main__':
+
+    '''
+    Cantidad de elementos de cada lista:
+    '''
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
